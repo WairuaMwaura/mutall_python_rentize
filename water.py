65e208080734ea0470897a18c47acf817fb37ab8
wairua <wairua.joshua@gmail.com>
2024-11-28 07:54:49 +0300

    add type hinting and new class for services

--- water.py
+++ water.py
@@ -1,10 +1,12 @@
-# File objective: Structure the 'wconnection_amount.py' file using a class
+# File objective 1: Structure the 'wconnection_amount.py' file using a class
+# File objective 2: Define another class for getting the service charges
 #
 # 1. Import libraries to use
 from mysql.connector import MySQLConnection, connect
 from mysql.connector.cursor import MySQLCursorDict
 import pandas as pd
 from pandas import DataFrame, merge
+from pandas.core.groupby.generic import DataFrameGroupBy
 from datetime import date, datetime
 #
 # 1.1. Set display options for pandas
@@ -55,61 +57,61 @@ def init() -> tuple[MySQLConnection, MySQLCursorDict]:
 #
 # 4. Define a class that encapsulates the water table
 class Water:
-    #
-    # 4.1. Define the constructor method
     def __init__(self):
         pass
-
+    #
+    # 4.1. Define the constructor method
     #
     # 4.2. Define the methods of the class
     # 4.2.1. Define method to get current water readings for each water
     #   connection
-    def get_current_readings(self) -> DataFrame:
+    @staticmethod
+    def get_current_readings() -> DataFrame:
         #
         # 4.2.1.1. Execute query on database
         kasa.execute("select * from wconnection")
         #
         # 4.2.1.2. Fetch and store the query result from database
-        all_water_connections = kasa.fetchall()
+        all_water_connections: list[dict] = kasa.fetchall()
         #
         # 4.2.1.3. Create a DataFrame from the result
-        all_water_connections_df = DataFrame(all_water_connections)
+        all_water_connections_df: DataFrame = DataFrame(all_water_connections)
         #
         # 4.4. Filter dataframe to have only active water connections
-        filter_date = date(9999, 12, 31)
-        active_water_connections_df = all_water_connections_df[
+        filter_date: date = date(9999, 12, 31)
+        active_water_connections_df: DataFrame = all_water_connections_df[
             all_water_connections_df['end_date'] == filter_date]
         #
         # 4.5. Execute query DB for all water meters
         kasa.execute("select * from wmeter")
         #
         # 4.6. Fetch the returning result
-        all_water_meters = kasa.fetchall()
+        all_water_meters: list[dict] = kasa.fetchall()
         #
         # 4.7. Create dataframe of water meters using Pandas
-        water_meters_df = DataFrame(all_water_meters)
+        water_meters_df: DataFrame = DataFrame(all_water_meters)
         #
         # 4.8. Execute query DB for all water readings
         kasa.execute("select * from wreading")
         #
         # 4.9. Fetch the returning result (i.e., a list of dictionaries|rows)
-        all_water_readings = kasa.fetchall()
+        all_water_readings: list[dict] = kasa.fetchall()
         #
         # 4.10. Create dataframe of water readings using Pandas
-        water_readings_df = DataFrame(all_water_readings)
+        water_readings_df: DataFrame = DataFrame(all_water_readings)
         #
         # 4.11. Join dataframes for water connection, water meter, and water
         # reading tables
         #
         # 4.11.1. Join the active water connections dataframe to the water
         # meters dataframe
-        water_connections_and_meters_df = merge(
+        water_connections_and_meters_df: DataFrame = merge(
             active_water_connections_df, water_meters_df, on='wmeter',
             how='inner'
         )
         # 4.11.2. Join the active water connections and water meters dataframe
         # to the water readings dataframe
-        all_water_connection_readings = merge(
+        all_water_connection_readings: DataFrame = merge(
             water_connections_and_meters_df,
             water_readings_df,
             on='wmeter',
@@ -118,25 +120,27 @@ class Water:
         #
         # 4.12. Get maximum date for each water connection
         # 4.12.1. Group dataframe by water connection
-        grouped_water_connections_df = all_water_connection_readings.groupby(
-            'wconnection'
+        grouped_water_connections_df: DataFrameGroupBy = (
+            all_water_connection_readings.groupby('wconnection')
         )
         #
         # 4.12.2. Get the row INDEX of the maximum water reading date for each
         # water connection
         # Index - identifier|label for each row in a dataframe
-        max_dates_indices = grouped_water_connections_df['date'].idxmax()
+        max_dates_indices: DataFrame = (
+            grouped_water_connections_df['date'].idxmax()
+        )
         #
         # 4.12.3. Use .loc to get the rows that match the indices above
-        latest_readings_df = all_water_connection_readings.loc[
+        latest_readings_df: DataFrame = all_water_connection_readings.loc[
             max_dates_indices]
         #
         # 4.13. Filter columns to show
-        latest_readings_df = latest_readings_df[
+        latest_readings_df: DataFrame = latest_readings_df[
             ['wconnection', 'serial_no', 'rate', 'date', 'value']
         ]
         # 4.14. Rename column names
-        latest_readings_df = latest_readings_df.rename(
+        latest_readings_df: DataFrame = latest_readings_df.rename(
             columns={
                 'date': 'curr_date',
                 'value': 'curr_value'
@@ -147,58 +151,65 @@ class Water:
     #
     # 4.2.2. Define method to get previous water readings for each water
     #   connection
-    def get_previous_readings(self) -> DataFrame:
+    @staticmethod
+    def get_previous_readings() -> DataFrame:
         # 5.1. Get last invoice period
         #
         # 5.1.1. Execute query on database
         kasa.execute("select * from period")
         #
         # 5.1.2. Fetch the results from Database
-        all_periods = kasa.fetchall()
+        all_periods: list[dict] = kasa.fetchall()
         #
         # 5.1.3. Create a Dataframe from the fetched result
-        periods_df = DataFrame(all_periods)
+        periods_df: DataFrame = DataFrame(all_periods)
         #
         # 5.1.4. Get today's date
-        leo = datetime.today()
+        leo: datetime = datetime.today()
         #
         # 5.1.5. Get previous invoice date
         # Based on today's date (month), get the period of last month
-        last_period_df = periods_df[
+        last_period_df: DataFrame = periods_df[
             (periods_df['month'] == leo.month - 1)
             & (periods_df['year'] == leo.year)
             ]
         # 5.1.6. Access cutoff date
-        cutoff_date = last_period_df.iloc[0]['cutoff']
+        cutoff_date: date = last_period_df.iloc[0]['cutoff']
         #
         # 5.2. Get water readings on based on the invoice|cutoff period
         # 5.2.1. Execute query on database
         kasa.execute("select * from water")
         #
         # 5.2.2. Fetch the result of the query from database
-        all_invoiced_water_readings = kasa.fetchall()
+        all_invoiced_water_readings: list[dict] = kasa.fetchall()
         #
         # 5.2.3. Create a dataframe from the fetched result
-        all_invoiced_water_readings_df = DataFrame(all_invoiced_water_readings)
+        all_invoiced_water_readings_df: DataFrame = DataFrame(
+            all_invoiced_water_readings
+        )
         #
         # 5.2.4. Filter the dataframe to have only rows of the last invoice date
         # |cutoff date
-        last_invoiced_water_readings_df = all_invoiced_water_readings_df[
+        last_invoiced_water_readings_df: DataFrame = all_invoiced_water_readings_df[
             all_invoiced_water_readings_df['curr_date'] == cutoff_date
             ]
         #
         # 5.3. Filter the columns to be displayed
-        last_invoiced_water_readings_df = last_invoiced_water_readings_df[
-            ['wconnection', 'curr_date', 'curr_value']
-        ]
+        last_invoiced_water_readings_df: DataFrame = (
+            last_invoiced_water_readings_df[
+                ['wconnection', 'curr_date', 'curr_value']
+            ]
+        )
         #
         # 5.4. Rename the column names
-        last_invoiced_water_readings_df = last_invoiced_water_readings_df.rename(
-            columns={
-                'curr_date': 'prev_date',
-                'curr_value': 'prev_value'
-            }
+        last_invoiced_water_readings_df: DataFrame = (
+            last_invoiced_water_readings_df.rename(columns={
+                    'curr_date': 'prev_date',
+                    'curr_value': 'prev_value'
+                }
+            )
         )
+
         return last_invoiced_water_readings_df
 
     #
@@ -207,7 +218,7 @@ class Water:
     def get_calculations(self) -> DataFrame:
         #
         # 6.1. Join the current readings and previous readings DataFrames
-        curr_and_prev_rds = merge(
+        curr_and_prev_rds: DataFrame = merge(
             self.get_previous_readings(),
             self.get_current_readings(),
             on='wconnection',
@@ -253,7 +264,7 @@ class Water:
                     'rate']
         )
         # 6.6. Reorder the columns
-        curr_and_prev_rds = curr_and_prev_rds[
+        curr_and_prev_rds: DataFrame = curr_and_prev_rds[
             ['wconnection', 'serial_no', 'prev_date', 'prev_value', 'curr_date',
              'curr_value', 'consumption_days', 'total_consumption',
              'avg_daily_cons', 'rate', 'amount']]
@@ -261,9 +272,81 @@ class Water:
 
 
 #
-# 5. Instantiate the class
-water_df = Water()
-#
-# 6. Call the calculation method to display|show the water table
-water_calculations = water_df.get_calculations()
+# Define a class for service charges calculations
+class Service:
+    #
+    # Define constructor method
+    def __init__(self):
+        pass
+
+    #
+    # Define method that gets current subscriptions for each client
+    @staticmethod
+    def get_service() -> DataFrame:
+        #
+        # Get list of all clients
+        kasa.execute("select name, client from client")
+        #
+        # Fetch results from database
+        all_clients: list[dict] = kasa.fetchall()
+        #
+        # Create a DataFrame from fetched result
+        clients_df: DataFrame = DataFrame(all_clients)
+        #
+        # Get active clients
+        # Join agreement table
+        kasa.execute(
+            """
+                select
+                    client,
+                    start_date, 
+                    `terminated`, 
+                    valid,
+                    agreement
+                from 
+                    agreement
+            """
+        )
+        #
+        # Fetch results from database
+        agreements: list[dict] = kasa.fetchall()
+        #
+        # Create a DataFrame for the agreements
+        agreements_df: DataFrame = DataFrame(agreements)
+        #
+        # Get active clients
+        # Join the clients and agreement DataFrames
+        clients_and_agreements_df: DataFrame = merge(
+            clients_df, agreements_df, on='client',
+            how='inner'
+        )
+        #
+        # Filter the joined client and agreement DataFrame
+        active_clients_df: DataFrame = clients_and_agreements_df[(clients_and_agreements_df['valid'] == 1) &
+                                                                 (clients_and_agreements_df['terminated'].isna())]
+
+        #
+        # Get subscription(s) for each active client
+        kasa.execute("select subscription, client, service from subscription")
+        subscriptions: list[dict] = kasa.fetchall()
+        subscriptions_df: DataFrame = DataFrame(subscriptions)
+        #
+        # Join the subscription and active clients DataFrames
+        client_subscriptions_df: DataFrame = merge(
+            active_clients_df, subscriptions_df, on='client', how='inner'
+        )
+        #
+        # Get the actual service details and join to the client-subscriptions DF
+        kasa.execute("select name, service, price, auto from service")
+        services: list[dict] = kasa.fetchall()
+        services_df: DataFrame = DataFrame(services)
+        #
+        # Join client-subscription and services DataFrames
+        client_sub_services_df: DataFrame = merge(
+            client_subscriptions_df, services_df, on='service', how='inner'
+        )
+        return client_sub_services_df
+
+client = Service()
+cl = client.get_service()
 print('f')
